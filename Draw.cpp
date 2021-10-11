0eafadf0d1317311c7a2a199db1f2a560bf63a2e
JosipOreskovic <josip.oreskovic@me.com>
2021-10-11 15:21:33 +0200

    Used for_each() and lambdas in Draw.pp

--- Draw.cpp
+++ Draw.cpp
@@ -12,48 +12,24 @@ void Game::draw() {
 
         background_.draw(window_, resolution_);
 
-        for (const auto& tile : tiles_)
-        {
-            if (tile->isActive())
-                tile->draw(window_);
-        }
+        for_each(tiles_.begin(), tiles_.end(), [&](auto& tile) { if (tile->isActive()) tile->draw(window_); });
 
         player.draw(window_);
 
-        for (const auto& ship : enemyFleet_)
-        {
-            if (ship->isActive())
-                ship->draw(window_);
-        }
-        for (const auto& meteor : meteors_)
-        {
-            if (meteor->isActive())
-                meteor->draw(window_);
-        }
-        for (const auto& rocket : rockets_)
-        {
-            if (rocket->isActive())
-                rocket->draw(window_);
-        }
-        for (const auto& tank : tanks_)
-        {
-            if (tank->isActive())
-                tank->draw(window_);
-        }
-        for (const auto& fuelTank : fuelTanks_)
-        {
-            if (fuelTank->isActive())
-                fuelTank->draw(window_);
-        }
-        for (const auto& mine : mines_)
-        {
-            if (mine->isActive())
-                mine->draw(window_);
-        }
+        for_each(enemyFleet_.begin(), enemyFleet_.end(), [&](auto& ship) { if (ship->isActive()) ship->draw(window_); });
+
+        for_each(meteors_.begin(), meteors_.end(), [&](auto& meteor) { if (meteor->isActive()) meteor->draw(window_); });
+
+		for_each(rockets_.begin(), rockets_.end(), [&](auto& rocket) { if (rocket->isActive()) rocket->draw(window_); });
+  
+		for_each(tanks_.begin(), tanks_.end(), [&](auto& tank) { if (tank->isActive()) tank->draw(window_); });
+
+        for_each(fuelTanks_.begin(), fuelTanks_.end(), [&](auto& fuelTank) { if (fuelTank->isActive()) fuelTank->draw(window_); });
+    
+        for_each(mines_.begin(), mines_.end(), [&](auto& mine) { if (mine->isActive()) mine->draw(window_); });
 
         window_.draw(livesText_);
         window_.draw(scoreText_);
-
     }
 
     if (state_ == State::Start)
